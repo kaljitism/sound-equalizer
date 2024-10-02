7969ec63a61495a9310511b2c4781cf9f358e056
kaljitism <dwikaljit@gmail.com>
2024-10-02 08:31:32 +0530

    tests

--- test/wav_test.dart
+++ test/wav_test.dart
@@ -12,8 +12,19 @@ void main() {
       WavFile wav = WavFile(openedFile);
 
       expect(wav.chunkId, equals('RIFF'));
-      expect(wav.chunkSize.runtimeType, equals(int));
+      expect(wav.chunkSize, equals(148170310));
       expect(wav.format, equals('WAVE'));
+      expect(wav.subChunk1ID, equals('fmt '));
+      expect(wav.subChunk1Size, equals(16));
+      expect(wav.audioFormat, equals(1));
+      expect(wav.numChannels, equals(2));
+      expect(wav.sampleRate, equals(44100));
+      expect(wav.byteRate, equals(176400));
+      expect(wav.blockAlign, equals(4));
+      expect(wav.bitsPerSample, equals(16));
+      expect(wav.extraParamSize, equals(0));
+      expect(wav.subChunk2ID, equals('LIST'));
+      expect(wav.subChunk2Size, equals(26));
     },
   );
 }
